d43a86732f4582d8c2b71dbdc874dce709d0cc47
msgabor <messziszabo@gmail.com>
2020-01-22 21:19:21 +0100

    while loop + infinite loop

--- 05_while_loop.py
+++ 05_while_loop.py
@@ -5,7 +5,10 @@ while number < 10:  # while the number is smaller then 10
     print("Number is smaller then 10. ")
     number += 1
 
+print("....................................")
+
+# infinite loop
 i = 1
 while i < 6:
-  print(i)
+    print(i)
 i += 1
