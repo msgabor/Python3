79fedf1d8de80627586551b961b783b29dcd9720
msgabor <messziszabo@gmail.com>
2020-01-22 20:59:45 +0100

    updated dict merge with tuple and list

--- 03b_dictionaries.py
+++ 03b_dictionaries.py
@@ -1,13 +1,15 @@
-
-name_age = {"Gabe": 34, "Leslie": 34, "George": 33}
+name_age = {"Gabe": 34,
+            "Leslie": 34,
+            "George": 33}
 
 for key in name_age.keys():
-    print(key)
-
+    print("This is the dict key-value pair 'key':", key)
+print("....................................")
 for value in name_age.values():
-    print(value)
-
+    print("This is the dict key-value pair 'value':", value)
+print("....................................")
 for key, value in name_age.items():
-    print(key, value)
+    print("This is the dict key-value pair 'key + value':", key, value)
 
 dict1 = {"data1": [1, 2, 3, 4], "data2": 78, "data3": (5, 6, 7, 8)}
+# nested dictionary with an tuples merged
