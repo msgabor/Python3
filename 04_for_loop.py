ef14533e15ce7d9360915d2527d8bab1dc10585a
msgabor <messziszabo@gmail.com>
2020-01-22 21:09:51 +0100

    updated dict merge with tuple and list

--- 04_for_loop.py
+++ 04_for_loop.py
@@ -1,23 +1,33 @@
 # well defined how many times want to loop
 for i in range(5):
     print("Gabe")
+
+print("....................................")
+
 range1 = range(35)
 print(list(range1))
+
+print("....................................")
 
 # list created - for loop on length of list1
 list1 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 for i in range(len(list1)):
     print(list1[i])
 
+print("....................................")
+
 # simple solution: for each
 for i in list1:
     print(i)
+
+print("....................................")
 
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
     if x == "banana":
         break
+print("....................................")
 
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
@@ -25,10 +35,14 @@ for x in fruits:
         continue
     print(x)
 
+print("....................................")
+
 for x in range(6):
     print(x)
 else:
     print("Finally finished!")
+
+print("....................................")
 
 # Nested for loops
 adj = ["red", "big", "tasty"]
@@ -38,11 +52,9 @@ for x in adj:
     for y in fruits:
         print(x, y)
 
-for x in [0, 1, 2]:
-    pass
-
 
 print("........enumeration..........")
+
 enum_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 for index, names in enumerate(enum_list):
     print(index, names)
