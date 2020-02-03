4a7fc3a6148fae343ebed1655464ce45bc1058a8
msgabor <messziszabo@gmail.com>
2020-02-03 10:16:00 +0100

    try/except/finally block are updated

--- 08_try_except_blocks.py
+++ 08_try_except_blocks.py
@@ -1,28 +1,21 @@
 try:
     print(x)
-except:
-    print("Something went wrong")
+except NameError as e:
+    print("Something went wrong:", e)
+except IndexError as e:
+    pass
 finally:
     print("The 'try except' is finished")
 
-# try:
-#     f = open("demofile.txt")
-#     f.write("Lorum Ipsum")
-# except:
-#     print("Something went wrong when writing to the file")
-# finally:
-#     f.close()
-#
-# x = -1
-#
-# if x < 0:
-#     raise Exception("Sorry, no numbers below zero")
-#
-# x = "hello"
-#
-# if not type(x) is int:
-#     raise TypeError("Only integers are allowed")
+print("*******************************************************")
 
-
-
+try:
+    f = open("demofile.txt")
+    f.write("Lorum Ipsum")
+except NameError as e:
+    pass
+except FileNotFoundError as e:
+    print("Something went wrong when writing to the file:", e)
+finally:
+    pass
 
