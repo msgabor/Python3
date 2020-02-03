1e87e1b4fb6ad3bc758a202eee899a47805eeda2
msgabor <messziszabo@gmail.com>
2020-02-03 10:47:33 +0100

    importing and namespaces/modules

--- 014_import_namespace_module(class).py
+++ 014_import_namespace_module(class).py
@@ -1,27 +1,26 @@
 import math
 from math import sin, cos
-
 print(sin(4))
 print(cos(3))
 
 print("............................................")
 
 import FirstClass
-import FirstClass as something
-from FirstClass import Class
-o = Class()
-
-FirstClass.call_function_from_another_file()
-
+FirstClass.call_function_from_another_file()  # calling function from another file --> namespace = another class/module
 print(FirstClass.color)
 print(FirstClass.names)
+
 print("............................................")
+
+import FirstClass as something                # --> if module are in another folder, can give alias name
 print(something.color)
 print(something.names)
 
+from FirstClass import Class                    # import just the Class from another module = now is empty Class
+o = Class()
+
+print("............................................")
 
 import datetime
-
 x = datetime.datetime.now()
-
 print(x)
