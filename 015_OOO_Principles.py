df9c795e5db5c4b366c61c7e5301e977e0e44e9d
msgabor <messziszabo@gmail.com>
2020-02-08 11:05:52 +0100

    added methods and setters

--- 015_OOO_Principles.py
+++ 015_OOO_Principles.py
@@ -1,19 +1,19 @@
 
-class Person: # class = plan for building, where house = more classes
+class Person:               # class = plan for building, where house = more classes
     # member of var/field - tagvaltozok
     name = ""
     age = None
     gender = ""
 
     # methods - inner class defined functions
-    def set_age(self, value):
-        self.age = value
+    def set_age(self, value):                   # self: a Class fenti age-jére mutat, tehát annak ad értéket
+        self.age = value                        # ha csak age = value lenne, akkor az nem a fenti age lenne a Class-ban
 
     def set_name(self, value):
         self.name = value
 
     def set_gender(self, value):
-        self.name = value
+        self.gender = value
 
 
 # object instance - objektum példány
@@ -25,6 +25,17 @@ person1.set_gender("man")
 print(person1.age)
 print(person1.name)
 print(person1.gender)
+
+print("******************************")
+
+person2 = Person()
+person2.set_age(30)
+person2.set_name("Csirke")
+person2.set_gender("female")
+print(person2.age)
+print(person2.name)
+print(person2.gender)
+
 
 # 001# of OOO = encapsulation
 '''Azt jelenti, hogy az adatstruktúrákat és az adott struktúrájú adatokat kezelő függvényeket (metódusokat) 
