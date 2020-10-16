21a5923933d618e9f178886e2100eefd7e9e5478
msgabor <messziszabo@gmail.com>
2020-10-16 13:52:13 +0200

    commit

---
+++ 002_operators.py
@@ -0,0 +1,30 @@
+# assignment
+# + * / - // ** %
+
+# comparison or relational operators
+# < > <= >= != ==
+
+# logical
+# || && !
+
+# Bitwise operator >> shifting
+
+num1 = 5
+num2 = 11
+
+# arithmetical operators
+print(num1 + num2)
+print(num2 - num1)
+print(num1 * num2)
+print(num1 / num2)
+print(num2 // num1)
+print(num1 ** num2)
+print(num2 % num1)
+
+# assignments
+# + - += -= *= /= //= %= **=
+num = 10
+num = num + 1
+num += 1
+num -= 1
+num *= 10
