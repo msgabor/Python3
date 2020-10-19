47f3f4fbc529a97f3475292fb11f26976c2bb68c
msgabor <messziszabo@gmail.com>
2020-10-19 11:23:40 +0200

    while loop

---
+++ 005_while_loop.py
@@ -0,0 +1,16 @@
+# esed, when we don't know how many times want to loop (million times)
+number = 0
+
+while number < 10:  # while the number is smaller then 10
+    print("Number is smaller then 10. ")
+    number += 1
+
+print("....................................")
+
+# infinite loop
+'''
+i = 1
+while i < 6:
+    print(i)
+i += 1
+'''
